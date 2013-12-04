db2f81134edb0d93097c512efcae695f4aa8a7fa
Kate <katepdonahue@gmail.com>
2013-12-04 14:49:35 -0500

    Write test for populate method

--- game/game_spec.rb
+++ game/game_spec.rb
@@ -2,7 +2,7 @@ require './game'
 require '../board/board'
 require '../cell/cell'
 
-# my_game = Game.new(10, 30)
+my_game = Game.new(10, 30)
 # my_game.screen.print_screen
 # sleep(0.5)
 # my_game.generation.print_screen
@@ -12,8 +12,16 @@ describe Game do
 
   describe "#create_cells" do
     it "should create 300 live cells upon initialization of a new game" do
-      my_game = Game.new(10, 30)
       expect(Cell::CELLS.count).to eq(300)
+    end
+  end
+
+  describe "#populate" do
+    it "should fill the screen with cell objects upon initialization" do
+      expect(my_game.screen.board[rand(0..9)][rand(0..29)].class).to eq(Cell)
+    end
+    it "should" do
+
     end
   end
 
